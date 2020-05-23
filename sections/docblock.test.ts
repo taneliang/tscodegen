cde6eb4b2a7a6b042d9e78c8fe0b5472b57c1cfc
E-Liang Tan <eliang@eliangtan.com>
2020-05-23 20:26:39 +0800

    Implement docblock section helpers

---
+++ sections/docblock.test.ts
@@ -0,0 +1,221 @@
+import { getFileDocblock, removeFileDocblock, prependFileDocblock } from './docblock';
+
+describe(getFileDocblock, () => {
+  test('should return undefined if code does not contain docblock', () => {
+    expect(getFileDocblock('')).toBeUndefined();
+    expect(
+      getFileDocblock(
+        `
+function add(a, b) {
+  return a + b;
+}
+        `.trim(),
+      ),
+    ).toBeUndefined();
+  });
+
+  test('should return undefined if code does not start with docblock', () => {
+    expect(
+      getFileDocblock(`
+/**
+ * File docblock with a newline above it
+ */
+      `),
+    ).toBeUndefined();
+  });
+
+  test('should return undefined if docblock is malformed', () => {
+    expect(
+      getFileDocblock(
+        `
+/*
+ * File "docblock" without the second star in the first line
+ */
+        `.trim(),
+      ),
+    ).toBeUndefined();
+    expect(
+      getFileDocblock(
+        `
+/**
+ File "docblock" without the leading star in a line
+ */
+        `.trim(),
+      ),
+    ).toBeUndefined();
+  });
+
+  test('should return docblock without leading stars', () => {
+    expect(
+      getFileDocblock(
+        `
+/**
+ * File docblock
+ * 
+ * More info
+ * 
+ * @partially-generated: Codelock<<aoaoreu9aoeu89aoe7u9ao7eu97oaoe98uaoe897u89>>
+ */
+
+/**
+ * Another docblock that should be ignored
+ */
+function add(a, b) {
+  return a + b;
+}
+        `.trim(),
+      ),
+    ).toBe(
+      `
+File docblock
+
+More info
+
+@partially-generated: Codelock<<aoaoreu9aoeu89aoe7u9ao7eu97oaoe98uaoe897u89>>
+      `.trim(),
+    );
+  });
+});
+
+describe(removeFileDocblock, () => {
+  function expectRemoveFileDocblockToPassthrough(code: string) {
+    expect(removeFileDocblock(code)).toBe(code);
+  }
+
+  test('should passthrough files without docblocks', () => {
+    expectRemoveFileDocblockToPassthrough('');
+    expectRemoveFileDocblockToPassthrough(
+      `
+function add(a, b) {
+  return a + b;
+}
+      `.trim(),
+    );
+  });
+
+  test('should passthrough files that does not start with docblock', () => {
+    expectRemoveFileDocblockToPassthrough(`
+/**
+ * File docblock with a newline above it
+ */
+    `);
+  });
+
+  test('should passthrough files with malformed docblocks', () => {
+    expectRemoveFileDocblockToPassthrough(
+      `
+/*
+ * File "docblock" without the second star in the first line
+ */
+      `.trim(),
+    );
+
+    expectRemoveFileDocblockToPassthrough(
+      `
+/**
+ File "docblock" without the leading star in a line
+ */
+      `.trim(),
+    );
+  });
+
+  test('should return code without docblock', () => {
+    expect(
+      removeFileDocblock(
+        `
+/**
+ * File docblock
+ * 
+ * More info
+ * 
+ * @partially-generated: Codelock<<aoaoreu9aoeu89aoe7u9ao7eu97oaoe98uaoe897u89>>
+ */
+
+/**
+ * Another docblock that should be ignored
+ */
+function add(a, b) {
+  return a + b;
+}
+        `.trim() + '\n', // Append newline to ensure we don't trim end
+      ),
+    ).toBe(
+      `
+/**
+ * Another docblock that should be ignored
+ */
+function add(a, b) {
+  return a + b;
+}
+`,
+    );
+  });
+});
+
+describe(prependFileDocblock, () => {
+  test('should prepend docblock correctly', () => {
+    expect(
+      prependFileDocblock(
+        `
+/**
+ * Another docblock that should be ignored
+ */
+function add(a, b) {
+  return a + b;
+}
+        `.trim() + '\n',
+        `
+File docblock
+
+More info
+
+@partially-generated: Codelock<<aoaoreu9aoeu89aoe7u9ao7eu97oaoe98uaoe897u89>>
+        `.trim(),
+      ),
+    ).toBe(
+      `
+/**
+ * File docblock
+ *
+ * More info
+ *
+ * @partially-generated: Codelock<<aoaoreu9aoeu89aoe7u9ao7eu97oaoe98uaoe897u89>>
+ */
+
+/**
+ * Another docblock that should be ignored
+ */
+function add(a, b) {
+  return a + b;
+}
+      `.trim() + '\n',
+    );
+  });
+});
+
+describe('reversibility', () => {
+  const docblockContent = `
+File docblock
+
+More info
+
+@partially-generated: Codelock<<aoaoreu9aoeu89aoe7u9ao7eu97oaoe98uaoe897u89>>
+  `.trim();
+
+  const code = `
+/**
+ * Another docblock that should be ignored
+ */
+function add(a, b) {
+  return a + b;
+}
+`;
+
+  test('prepend -> get should return same docblock content', () => {
+    expect(getFileDocblock(prependFileDocblock(code, docblockContent))).toBe(docblockContent);
+  });
+
+  test('prepend -> remove should return original code', () => {
+    expect(removeFileDocblock(prependFileDocblock(code, docblockContent))).toBe(code);
+  });
+});
---
+++ sections/docblock.ts
@@ -0,0 +1,59 @@
+const docblockMatchRegExp = /^\/\*\*\n(?<contents>( \*.*\n)*?) \*\/\n/;
+
+/**
+ * Get file docblock from `code`, the contents of a source file.
+ *
+ * Assumes that any docblock that starts from the 0th character in `code` is
+ * the file docblock.
+ *
+ * @param code Code from a source file.
+ * @returns Docblock if it exists, else returns undefined.
+ */
+export function getFileDocblock(code: string): string | undefined {
+  const rawDocblockMatch = code.match(docblockMatchRegExp);
+  if (!rawDocblockMatch) {
+    return undefined;
+  }
+
+  const rawContents = rawDocblockMatch.groups!.contents;
+  return (
+    rawContents
+      // Remove each line's leading " *"
+      .split('\n')
+      .map((line) => line.substring(' *'.length).trim())
+      .join('\n')
+      // Remove trailing \n
+      .trim()
+  );
+}
+
+/**
+ * Removes the file docblock from the start of a file.
+ *
+ * Assumes that any docblock that starts from the 0th character in `code` is
+ * the file docblock.
+ *
+ * @param code Code from a source file.
+ */
+export function removeFileDocblock(code: string): string {
+  if (!code.match(docblockMatchRegExp)) {
+    return code;
+  }
+  return code.replace(docblockMatchRegExp, '');
+}
+
+/**
+ * Prepends `code` with `dockblockContent`. Assumes `code` does not already
+ * have a file docblock.
+ *
+ * @param code Code from a source file.
+ * @param docblockContent Plain docblock content (i.e. without "*"s at the start of each line)
+ */
+export function prependFileDocblock(code: string, docblockContent: string): string {
+  const docblockContentWithLeadingStars = docblockContent
+    .split('\n')
+    .map((line) => ` ${`* ${line}`.trim()}`) // " *" for empty lines, ` * ${line}` otherwise
+    .join('\n');
+  const docblock = `/**\n${docblockContentWithLeadingStars}\n */`;
+  return `${docblock}\n\n${code.trimStart()}`;
+}
