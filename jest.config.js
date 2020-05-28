02dcefb7c37ab73699db845626dd13a676052616
E-Liang Tan <eliang@eliangtan.com>
2020-05-28 14:47:18 +0800

    [Extract open source] Add package.json, Git, TS, Jest configs

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+// For a detailed explanation regarding each configuration property, visit:
+// https://jestjs.io/docs/en/configuration.html
+
+module.exports = {
+  // A preset that is used as a base for Jest's configuration
+  preset: "ts-jest",
+  // The test environment that will be used for testing
+  testEnvironment: "node",
+};
